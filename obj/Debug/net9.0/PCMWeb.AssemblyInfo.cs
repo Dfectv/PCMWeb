bef5abb5dee942749c1f670cc0531786e3f83b57
Ben Paepke <psilopsyben@gmail.com>
2025-04-14 19:48:41 -0700

    About Us page looks god

--- obj/Debug/net9.0/PCMWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/PCMWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PCMWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6adbf023b7a0ee0fe73bccb23797aa3f008867bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9a9eedb71acb884cbab5021da6b4e2cb614482")]
 [assembly: System.Reflection.AssemblyProductAttribute("PCMWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PCMWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
