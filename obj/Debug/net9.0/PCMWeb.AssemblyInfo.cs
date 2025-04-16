d56fd21099aff6b91e6aafacefeab7bda917d3b7
Ben Paepke <psilopsyben@gmail.com>
2025-04-15 18:05:13 -0700

    Amos page essentially done

--- obj/Debug/net9.0/PCMWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/PCMWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PCMWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479955553887cb660650c01dbeec9ea73b8d1fed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4451a4e79af30755d1bd9e5860ae06c9341f752b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PCMWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PCMWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
